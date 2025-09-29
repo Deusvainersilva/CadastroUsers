9ed0c9e36e37fecf495a57f64b2eadca6f09bbdb
Deusvainersilva <deusvainerjuli@gmail.com>
2025-09-29 00:59:02 -0300

    Comittando a controller

---
+++ src/main/java/silva/deusvainer/CadastroUsuario/Controller.java
@@ -0,0 +1,17 @@
+package silva.deusvainer.CadastroUsuario;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+
+public class Controller {
+
+    @GetMapping("/primeiramensagem")
+    public String primeiraMensagem(){
+        return "Primeira mensagem nessa rota.";
+    }
+
+}
